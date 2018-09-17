15a9d59534fb49a780b004dde7cbfde391c64b91
Oleksii Bochurkin <oleksii.bochurkin@echovox.com>
2018-09-17 12:56:20 +0300

    added some dummy validation

--- src/password.js
+++ src/password.js
@@ -3,9 +3,14 @@ const SPECIAL_CHARS_SET = '.,/-&?$#@!*<>'
 const CHARS_SET = 'abcdefghijklmnopqrstuvwxyz'
 const NUMBER_SET = '0123456789'
 
-const isValid = (...params) => {
-  const [length, ...other] = params
-  if ((length < 8) && (other.reduce((acc, curr) => acc + curr, 0) > length)) {
+const isValid = async (...params) => {
+  const [length, uppercase, digits, special] = params
+  if (isNaN(length)) throw new Error(`Parameter length is invalid. Expected int, got ${length}`)
+  if (isNaN(uppercase)) throw new Error(`Parameter uppercase is invalid. Expected int, got ${uppercase}`)
+  if (isNaN(digits)) throw new Error(`Parameter digits is invalid. Expected int, got ${digits}`)
+  if (isNaN(special)) throw new Error(`Parameter special is invalid. Expected int, got ${special}`)
+
+  if ((length < 8) && ([uppercase, digits, special].reduce((acc, curr) => acc + curr, 0) > length)) {
     throw new Error(`password length can not be less then ${length}`)
   }
   return true
@@ -29,7 +34,7 @@ const getSpecialChars = async () => {
 
 class Password {
   static async genarate (length, uppercase, digits, special) {
-    isValid(length, uppercase, digits, special)
+    await isValid(length, uppercase, digits, special)
     let passChars = []
     const lowerCaseLength = length - (uppercase + digits + special)
     for (let i = 0; i < lowerCaseLength; i++) {
